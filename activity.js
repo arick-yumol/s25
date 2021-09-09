e3f0d8c89598ee610df209bd3759f20065522024
arick_yumol <arick.yumol@gmail.com>
2021-09-09 22:11:45 +0800

    Fixed activity codes

--- activity.js
+++ activity.js
@@ -11,6 +11,11 @@ db.fruits.aggregate( [
 		$count: "fruit_count_on_sale"
 	} 
 ] );
+
+db.fruits.aggregate( [ 
+	{ $match: { onSale: true } }, 
+	{ $count: "fruit_count_on_sale" } 
+] );
 ================== 2 ==================
 
 ================== 3 ==================
@@ -26,6 +31,11 @@ db.fruits.aggregate( [
 		$count: "fruit_stock_above_20"
 	} 
 ] );
+
+db.fruits.aggregate( [ 
+	{ $match: { stock: { $gt: 20 } } }, 
+	{ $count: "fruit_stock_above_20" } 
+] );
 ================== 3 ==================
 
 ================== 4 ==================
@@ -34,50 +44,82 @@ db.fruits.aggregate([
         $match: { 
             onSale: true
          }
-	}, 
-    {
-        $unwind: "$origin"
-    }, 
+	},
 	{ 
         $group: {
-            _id: "$origin",
+            _id: "$supplier_id",
             avg_price: {
                 $avg: "$price"
             }
         }
 	} 
 ]);
+
+db.fruits.aggregate([ 
+	{ 
+		$match: { 
+			onSale: true 
+		} 
+	}, 
+	{ 
+		$group: { 
+			_id: "$supplier_id", 
+			avg_price: { 
+				$avg: "$price" 
+			} 
+		} 
+	} 
+]);
+
+db.fruits.aggregate([ 
+	{ $match: { onSale: true } }, 
+	{ $group: { _id: "$supplier_id", avg_price: { $avg: "$price" } } } 
+]);
 ================== 4 ==================
 
 ================== 5 ==================
 db.fruits.aggregate([ 
-    {
-        $unwind: "$origin"
+    { 
+    	$match: { 
+    		onSale: true 
+    	} 
     }, 
 	{ 
-        $group: {
-            _id: "$origin",
-            highest_price: {
-                $max: "$price"
-            }
-        }
+		$group: { 
+			_id: "$supplier_id", 
+			highest_price: { 
+				$max: "$price" 
+			} 
+		} 
 	} 
+]);
+
+db.fruits.aggregate([ 
+    { $match: { onSale: true } }, 
+	{ $group: { _id: "$supplier_id", highest_price: { $max: "$price" } } } 
 ]);
 ================== 5 ==================
 
 ================== 6 ==================
 db.fruits.aggregate([ 
-    {
-        $unwind: "$origin"
+    { 
+    	$match: { 
+    		onSale: true 
+    	} 
     }, 
 	{ 
-        $group: {
-            _id: "$origin",
-            lowest_price: {
-                $min: "$price"
-            }
-        }
+		$group: { 
+			_id: "$supplier_id", 
+			lowest_price: { 
+				$min: "$price" 
+			} 
+		} 
 	} 
+]);
+
+db.fruits.aggregate([ 
+    { $match: { onSale: true } }, 
+	{ $group: { _id: "$supplier_id", lowest_price: { $min: "$price" } } } 
 ]);
 ================== 6 ==================
 
